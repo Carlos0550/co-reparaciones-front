66b9ed7e64522489bff9edff11953f56684e0f9e
carlos0550 <carlospelinski03@gmail.com>
2024-12-21 00:28:53 -0300

    Se realizan modificaciones generales

--- src/Admin/pages/ProductManager/Forms/AddProducts.jsx
+++ src/Admin/pages/ProductManager/Forms/AddProducts.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Form, Input, Select, Upload, message, Button, Space } from 'antd';
-import { InboxOutlined } from '@ant-design/icons';
+import { PlusOutlined } from '@ant-design/icons';
 import { useAppContext } from '../../../../AppContext';
 import { v4 as uuidv4 } from 'uuid';
 import { resizeAndConvertImages } from '../../../../utils/ResizeImages';
@@ -10,12 +10,10 @@ import { stateToHTML } from 'draft-js-export-html';
 import { Editor } from 'react-draft-wysiwyg';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
-const { Dragger } = Upload;
-
 function AddProducts() {
     const [form] = Form.useForm();
-    const { categories, saveProduct, getProducts, editingProduct, 
-        productId, handleProducts, productsList, editProducts
+    const { categories, saveProduct, getProducts, editingProduct,
+        productId, handleProducts, productsList, editProducts, getCategories
 
     } = useAppContext();
 
@@ -24,53 +22,45 @@ function AddProducts() {
 
     const onEditorStateChange = (newState) => {
         setEditorState(newState);
-        const rawContext = convertToRaw(newState.getCurrentContent());
-        const plainText = rawContext.blocks.map(block => block.text).join('\n');
-        form.setFieldsValue({
-            product_description: plainText,
-        });
+
     };
-    const [uploadProps, setUploadProps] = useState({name: 'file',
-        multiple: true,
-        beforeUpload: async (file) => {
-            try {
-                const [compressedFile] = await resizeAndConvertImages([file]);
 
-                const newFileList = {
-                    uid: uuidv4(),
-                    name: compressedFile.name,
-                    status: 'success',
-                    originFileObj: compressedFile,
-                    editing: false,
-                    thumbUrl: URL.createObjectURL(compressedFile),
-                };
+    const handleDeleteImage = (file) => {
+        setFileList((prevList) => prevList.filter((item) => item.name.split(".")[0] !== file.name.split(".")[0]));
+    }
 
-                setFileList((prevList) => [...prevList, newFileList]);
-            } catch (error) {
-                message.error('Error al redimensionar la imagen');
-                console.error('Error al redimensionar la imagen:', error);
+    const beforeUpload = async (file) => {
+        const isImage = file.type.startsWith("image/")
+        if (!isImage) {
+            form.setFields([
+                {
+                    name: "product_images",
+                    errors: ["Solo se permiten archivos de imagen"]
+                }
+            ]);
+            return Upload.LIST_IGNORE
+        } else {
+            const [processedImages] = await resizeAndConvertImages([file])
+
+            const newFileList = {
+                uid: processedImages.uid,
+                name: processedImages.name,
+                originFileObj: processedImages,
+                editing: false,
+                thumbUrl: URL.createObjectURL(processedImages)
             }
+            setFileList((prevList) => [...prevList, newFileList]);
+        }
+        return false
+    }
 
-            return false; 
-        },
-        fileList,
-        onRemove: (file) => {
-            setFileList((prevList) => prevList.filter((item) => item.name.split(".")[0] !== file.name.split(".")[0]));
-        },});
-    
-        useEffect(()=>{
-            setUploadProps((prevProps) => ({
-                ...prevProps,
-                fileList, 
-            }));
-        },[fileList])
     const [saving, setSaving] = useState(false);
     const onFinish = async (values) => {
         setSaving(true)
         const htmlDescription = stateToHTML(editorState.getCurrentContent());
         const formData = new FormData();
         for (const key in values) {
-            if(key !== "product_images" && key !== "product_description") {
+            if (key !== "product_images" && key !== "product_description") {
                 formData.append(key, values[key] || "");
             }
         }
@@ -83,20 +73,20 @@ function AddProducts() {
 
 
         fileList.forEach((file) => {
-            if(!file.editing){
+            if (!file.editing) {
                 formData.append("images", file.originFileObj);
             }
         })
 
         formData.append("imagesWithEdit", JSON.stringify(imagesWithEdit));
 
-        const result = editingProduct 
-        ? await editProducts(formData, productId) 
-        : await saveProduct(formData)
+        const result = editingProduct
+            ? await editProducts(formData, productId)
+            : await saveProduct(formData)
 
-        if(result){
-            message.success(editingProduct 
-                ? 'Producto editado con éxito' 
+        if (result) {
+            message.success(editingProduct
+                ? 'Producto editado con éxito'
                 : 'Producto registrado con éxito'
             );
 
@@ -110,16 +100,16 @@ function AddProducts() {
         setSaving(false)
     };
 
-    useEffect(()=>{
-        if(editingProduct && productId){
+    useEffect(() => {
+        if (editingProduct && productId) {
             const selectedProduct = productsList.find(product => product.id === productId)
             form.setFieldsValue({
                 product_name: selectedProduct.product_name,
                 product_category: selectedProduct.product_category,
                 product_price: selectedProduct.product_price,
                 product_description: selectedProduct.product_description,
-                product_stock: selectedProduct.stock
-                
+                product_stock: selectedProduct.stock,
+
             })
             const blockFromHTML = convertFromHTML(selectedProduct.product_description);
             const contentState = ContentState.createFromBlockArray(blockFromHTML.contentBlocks, blockFromHTML.entityMap);
@@ -132,25 +122,20 @@ function AddProducts() {
                 status: 'done',
                 editing: true,
                 originFileObj: new File([image.image_data], image.image_name, { type: image.image_type }),
-                
+                thumbUrl: image.image_data
             }))
-    
+
             setFileList(formattedImages)
-            
-        }else{
+            form.setFieldsValue({
+                product_images: formattedImages
+            })
+        } else {
             form.resetFields()
             setFileList([])
             setEditorState(EditorState.createEmpty())
 
         }
-    },[editingProduct, productId])
-
-useEffect(()=>{
-    form.setFieldsValue({
-        product_images: fileList
-    })
-},[fileList])
-
+    }, [editingProduct, productId])
 
     return (
         <Form
@@ -169,64 +154,74 @@ useEffect(()=>{
             </Form.Item>
 
             <Space wrap align='center'>
-            <Form.Item
-                name="product_price"
-                label="Precio"
-                rules={[{ required: true, message: 'Por favor ingrese el precio' }]}
-            >
-                <Input placeholder='Ingrese el precio' />
-            </Form.Item>
+                <Form.Item
+                    name="product_price"
+                    label="Precio"
+                    rules={[{ required: true, message: 'Por favor ingrese el precio' }]}
+                >
+                    <Input placeholder='Ingrese el precio' />
+                </Form.Item>
 
-            <Form.Item
-                name={"product_stock"}
-                label="Stock disponible"
-                rules={
-                    [
-                        { required: true, message: 'Por favor ingrese el stock disponible' },
-                        {validator:(_,value) => {
-                            if(!/^(0|[1-9]\d*)$/.test(value)) return Promise.reject(new Error('El stock disponible no puede ser negativo ni incluir letras.'))
+                <Form.Item
+                    name={"product_stock"}
+                    label="Stock disponible"
+                    rules={
+                        [
+                            { required: true, message: 'Por favor ingrese el stock disponible' },
+                            {
+                                validator: (_, value) => {
+                                    if (!/^(0|[1-9]\d*)$/.test(value)) return Promise.reject(new Error('El stock disponible no puede ser negativo ni incluir letras.'))
 
-                            return Promise.resolve()
-                        }}
-                    ]
-                }
-            >
-                <Input placeholder='Ingrese el stock disponible'/>
-            </Form.Item>
-
-            <Form.Item
-                name="product_category"
-                label="Categoría"
-                rules={[{ required: true, message: 'Por favor seleccione la categoría' }]}
-            >
-                {categories.length > 0 ? (
-                    <Select placeholder="Seleccione una categoría">
-                        {categories.map((category) => (
-                            <Select.Option key={category.id} value={category.id}>
-                                {category.category_name}
-                            </Select.Option>
-                        ))}
-                    </Select>
-                ) : (
-                    <p>No hay categorías disponibles.</p>
-                )}
-            </Form.Item>
+                                    return Promise.resolve()
+                                }
+                            }
+                        ]
+                    }
+                >
+                    <Input placeholder='Ingrese el stock disponible' />
+                </Form.Item>
+                <Button onClick={async () => {
+                    const hiddenMessage = message.loading('Actualizando categorías...')
+                    await getCategories()
+                    hiddenMessage()
+                }}>Re obtener categorías</Button>
+                <Form.Item
+                    name="product_category"
+                    label="Categoría"
+                    rules={[{ required: true, message: 'Por favor seleccione la categoría' }]}
+                >
+                    {categories.length > 0 ? (
+                        <Select placeholder="Seleccione una categoría">
+                            {categories.map((category) => (
+                                <Select.Option key={category.id} value={category.id}>
+                                    {category.category_name}
+                                </Select.Option>
+                            ))}
+                        </Select>
+                    ) : (
+                        <p>No hay categorías disponibles.</p>
+                    )}
+                </Form.Item>
             </Space>
 
             <Form.Item
                 name="product_description"
                 label="Descripción"
-                
+
                 rules={[{ required: true, message: 'Por favor ingrese la descripción' }]}
             >
+
                 <Editor
                     editorState={editorState}
                     onEditorStateChange={onEditorStateChange}
-                    placeholder='Ingrese la descripción del producto'
-                    
+                    placeholder="Ingrese la descripción del producto"
                     toolbar={{
-                        options: ['inline', 'list', 'textAlign', 'link', 'history'],
+                        options: ["inline", "list", "textAlign", "link", "history"],
+                        inline: {
+                            options: ["bold", "italic", "underline"]
+                        }
                     }}
+
                 />
             </Form.Item>
 
@@ -235,18 +230,19 @@ useEffect(()=>{
                 label="Imágenes"
                 rules={[{ required: true, message: 'Por favor suba al menos una imagen' }]}
                 valuePropName="fileList"
-               
+
                 getValueFromEvent={(e) => e && e.fileList}
             >
-                <Dragger {...uploadProps} fileList={fileList} maxCount={3}>
-                    <p className="ant-upload-drag-icon">
-                        <InboxOutlined />
-                    </p>
-                    <p className="ant-upload-text">Haga clic o arrastre archivos a esta área para subir</p>
-                    <p className="ant-upload-hint">
-                        Soporte para una o varias imágenes. No suba archivos prohibidos o datos confidenciales.
-                    </p>
-                </Dragger>
+                <Upload
+                    accept='image/jpeg, image/png'
+                    listType='picture-card'
+                    fileList={fileList}
+                    onRemove={handleDeleteImage}
+                    beforeUpload={beforeUpload}
+                    multiple
+                >
+                    {fileList.length < 4 && <Button icon={<PlusOutlined />} />}
+                </Upload>
             </Form.Item>
 
             <Form.Item>
