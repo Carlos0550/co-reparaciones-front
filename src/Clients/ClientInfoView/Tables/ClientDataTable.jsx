c63623e138c5220cdf1448b3df478eea2aabc27e
carlos0550 <carlospelinski03@gmail.com>
2025-01-05 16:55:44 -0300

    ajustes menores

--- src/Clients/ClientInfoView/Tables/ClientDataTable.jsx
+++ src/Clients/ClientInfoView/Tables/ClientDataTable.jsx
@@ -1,5 +1,5 @@
 import { Button, Tag, Table } from 'antd'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useAppContext } from '../../../AppContext'
 import { EditOutlined, IdcardOutlined, MailOutlined, PhoneOutlined, SmileOutlined, UserOutlined } from '@ant-design/icons'
 import RoadIcon from "../../../../public/road_24dp_EFEFEF_FILL0_wght400_GRAD0_opsz24.svg"
@@ -132,11 +132,38 @@ function ClientDataTable() {
             )
         }
     ]
+
+    const [clientData, setClientData] = useState([])  
+
+    useEffect(()=>{
+        if(loginData && loginData[0]){
+            const customerData = [
+                {
+                    auth_code: loginData[0]?.auth_code || null,
+                    city: loginData[0]?.city || null,
+                    client_uuid: loginData[0]?.client_uuid || null,
+                    dni: loginData[0]?.dni || null,
+                    first_address: loginData[0]?.first_address || null,
+                    id: loginData[0]?.id || null,
+                    is_verified: loginData[0]?.is_verified || null,
+                    postal_code: loginData[0]?.postal_code || null,
+                    province: loginData[0]?.province || null,
+                    second_address: loginData[0]?.second_address || null,
+                    session_timeout: loginData[0]?.session_timeout || null,
+                    user_email: loginData[0]?.user_email || null,
+                    user_fullname: loginData[0]?.user_fullname || null,
+                    user_phone: loginData[0]?.user_phone || null
+                }
+            ];
+            setClientData(customerData)
+        }
+
+    },[loginData])
   return (
     <React.Fragment>
         <Table
         columns={tableCols}
-        dataSource={loginData}
+        dataSource={clientData}
 
         pagination={false}
         rowKey={(record) => record.id}
