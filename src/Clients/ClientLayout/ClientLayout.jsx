3c09b7e74f99aef63f036e7e681dd8ff1b1884c4
carlos0550 <carlospelinski03@gmail.com>
2025-01-06 16:14:41 -0300

    Muestra mas productos por pagina

--- src/Clients/ClientLayout/ClientLayout.jsx
+++ src/Clients/ClientLayout/ClientLayout.jsx
@@ -38,7 +38,7 @@ function ClientLayout() {
     }, [])
 
     const [currentPage, setCurrentPage] = useState(1)
-    const [productsPerPage] = useState(10);
+    const [productsPerPage] = useState(20);
 
     const indexOfLastProduct = currentPage * productsPerPage
     const indexOfFirstProduct = indexOfLastProduct - productsPerPage
